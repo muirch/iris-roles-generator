c1077cceb3bcfb8eb3371e930cce0d47a860eac0
Ivan Muir <mimuir@ya.ru>
2020-10-27 18:06:54 +0300

    added description to the nav

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,20 @@
       <b-navbar variant="faded" type="light" class="text-center">
         <b-navbar-brand tag="h1" class="m-auto">Помощник-генератор ролей {{ version }}</b-navbar-brand>
       </b-navbar>
+      <b-container class="description">
+        <label>Данный инструмент предоставляет удобный интерфейс для редактирования ролей беседы с ботом <a href="https://vk.com/iris_cm" target="_blank">Iris</a>. Для того, чтобы ознакомиться с инструкцией, <a href="#" v-b-modal.modal-center>нажмите сюда</a>.</label>
+        <b-modal id="modal-center" centered title="Инструкция">
+          <p>Все довольно просто:</p>
+          <ul>
+            <li>Заполните все поля</li>
+            <li>Перейдите на вкладку результат и нажмите кнопку скопировать</li>
+            <li>Вернитесь в беседу с ботом Iris и отправьте скопированную команду</li>
+          </ul>
+          <template #modal-footer>
+              <b-button variant="primary" size="md" class="w-25 float-right" block @click="$bvModal.hide('modal-center')">Понятно</b-button>
+          </template>
+        </b-modal>
+      </b-container>
     </div>
     <router-view/>
   </div>
@@ -28,6 +42,10 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
+}
+
+.description {
+  max-width: 800px !important;
 }
 
 .input-group {
